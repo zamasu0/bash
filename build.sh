3450f48d4391ca7e94e088daac0e4ea5bee686dd
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-07 14:21:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Remove existing directories
 rm -rf device/oneplus/fajita
 rm -rf device/oneplus/sdm845-common
-rm -rf device/derp/sepolicy
+rm -rf device/lineage/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
 rm -rf kernel/oneplus/sdm845
 rm -rf vendor/oneplus/fajita
@@ -14,20 +14,20 @@ rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
 
 # Clone repositories
-git clone https://github.com/zamasu0/android_device_oneplus_fajita_4.19 --depth=1 device/oneplus/fajita
-git clone https://github.com/zamasu0/android_device_oneplus_sdm845-common_4.19_los --depth=1 device/oneplus/sdm845-common
+git clone https://github.com/zamasu0/android_device_oneplus_fajita_4.19 --depth=1 -b lineage-21 device/oneplus/fajita
+git clone https://github.com/zamasu0/android_device_oneplus_sdm845-common_4.19_los --depth=1 -b lineage-21 device/oneplus/sdm845-common
 git clone https://github.com/EdwinMoq/android_kernel_oneplus_sdm845 --depth=1 kernel/oneplus/sdm845
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
 git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19 --depth=1 vendor/oneplus/sdm845-common
 git clone https://github.com/LineageOS/android_hardware_oneplus --depth=1 hardware/oneplus
-git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/derp/sepolicy
+git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/lineage/sepolicy
 git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 device/qcom/sepolicy_vndr/legacy-um
 git clone https://github.com/zamasu0/media --depth=1 hardware/qcom-caf/sdm845/media
 git clone https://github.com/zamasu0/audio --depth=1 hardware/qcom-caf/sdm845/audio
 git clone https://github.com/zamasu0/display --depth=1 hardware/qcom-caf/sdm845/display
 
 # build-setup
-export DERP_BUILD=true
+export LINEAGE_BUILD=true
 source build/envsetup.sh
-lunch derp_fajita-userdebug
+lunch lineage_fajita-ap2a-eng
 m bacon
