5a6884f08da7582bc0427db34d25c14d911d664e
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-08 08:55:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ rm -rf hardware/oneplus
 rm -rf hardware/qcom-caf/sdm845/media
 rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
-rm -rf frameworks/native
 
 # Clone repositories
 git clone https://github.com/zamasu0/android_device_oneplus_fajita_4.19 --depth=1 -b derp-14 device/oneplus/fajita
@@ -29,7 +28,6 @@ git clone https://github.com/zamasu0/display --depth=1 hardware/qcom-caf/sdm845/
 git clone https://github.com/Linux-on-droid/vendor_lindroid --depth=1 vendor/lindroid
 git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b staging/lindroid-21 external/libhybris
 git clone https://github.com/Linux-on-droid/external_lxc --depth=1 external/lxc
-git clone https://github.com/zamasu0/android_frameworks_native --depth=1 frameworks/native
 
 # build-setup
 export DERP_BUILD=true
