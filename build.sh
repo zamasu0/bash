05fa5e563b1da2282fe3d78920f218f7053c8936
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-08 08:38:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Remove existing directories
 rm -rf device/oneplus/fajita
 rm -rf device/oneplus/sdm845-common
-rm -rf device/lineage/sepolicy
+rm -rf device/derp/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
 rm -rf kernel/oneplus/sdm845
 rm -rf vendor/oneplus/fajita
@@ -21,7 +21,7 @@ git clone https://github.com/EdwinMoq/android_kernel_oneplus_sdm845 --depth=1 ke
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 vendor/oneplus/fajita
 git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19 --depth=1 vendor/oneplus/sdm845-common
 git clone https://github.com/LineageOS/android_hardware_oneplus --depth=1 hardware/oneplus
-git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/lineage/sepolicy
+git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/derp/sepolicy
 git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 device/qcom/sepolicy_vndr/legacy-um
 git clone https://github.com/zamasu0/media --depth=1 hardware/qcom-caf/sdm845/media
 git clone https://github.com/zamasu0/audio --depth=1 hardware/qcom-caf/sdm845/audio
