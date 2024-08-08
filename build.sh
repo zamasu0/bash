b2e4abe0bf74aaf39aeefe195bdc94553bd1eb1d
Zamasu <129572513+zamasu0@users.noreply.github.com>
2024-08-08 15:20:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --dept
 git clone https://github.com/zamasu0/proprietary_vendor_oneplus_sdm845-common_4.19 --depth=1 vendor/oneplus/sdm845-common
 git clone https://github.com/LineageOS/android_hardware_oneplus --depth=1 hardware/oneplus
 git clone https://github.com/zamasu0/android_device_lineage_sepolicy_4.19_los --depth=1 device/derp/sepolicy
-git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 device/qcom/sepolicy_vndr/legacy-um
+git clone https://github.com/zamasu0/android_device_qcom_sepolicy_vndr_4.19_los --depth=1 -b derp-legacy-um device/qcom/sepolicy_vndr/legacy-um
 git clone https://github.com/zamasu0/media --depth=1 hardware/qcom-caf/sdm845/media
 git clone https://github.com/zamasu0/audio --depth=1 hardware/qcom-caf/sdm845/audio
 git clone https://github.com/zamasu0/display --depth=1 hardware/qcom-caf/sdm845/display
